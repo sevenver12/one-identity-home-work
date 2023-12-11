42d31188f7a5a6de74e762d040ed417b97444a06
Bence Szalma <bence.szalma@mxcsoftware.it>
2023-12-11 01:43:47 +0100

    Code smells

--- src/OneIdentity.Homework.Database/EfContext.cs
+++ src/OneIdentity.Homework.Database/EfContext.cs
@@ -6,30 +6,18 @@ namespace OneIdentity.Homework.Database;
 
 public class EfContext : DbContext
 {
-    public EfContext() { }
     public EfContext(DbContextOptions options) : base(options) { }
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        base.OnConfiguring(optionsBuilder);
-    }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        //base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<User>(u =>
         {
             u.OwnsOne(x => x.Address,c=>c.OwnsOne(address=>address.Geo));
             u.OwnsOne(x => x.Company).ToJson();
         });
-        //modelBuilder.Entity<Address>(a=>
-        //{
-        //    a.OwnsOne(x => x.Geo).ToJson();
-        //});
-        //modelBuilder.Entity<Company>();
-        //modelBuilder.Entity<Geo>();
+
         /// Can't really configure here and no way to tell ef how to generate Ids the recommended way in mongo is to use ObjectId or Guid so i chose Guid
 
     }
